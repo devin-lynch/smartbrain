44723f5695cc2b5d07b76570c2439eda2187120e
devin-lynch <devin.l.lynch@gmail.com>
2023-02-02 20:11:35 -0600

    Add temp bg

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
+      {/* <Logo /> */}
+      {/* <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navigation() {
     return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p>Sign Out</p>
+            <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
         </nav>
     )
 }
