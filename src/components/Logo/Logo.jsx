ba8cb02adf94d097a259f61e11198902ffbaef44
devin-lynch <devin.l.lynch@gmail.com>
2023-02-03 17:37:15 -0600

    Add logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.png'
+import './Logo.css'
 
 export default function Logo() {
     return (
         <div className="ma4 mt0">
-            <Tilt>
-                <div style={{ height: '300px', backgroundColor: 'darkgreen' }}>
-                    <h1>React Parallax Tilt 👀</h1>
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }}>
+                <div className="Tilt-inner pa3">
+                    <img style={{ paddingTop: '5px' }} src={brain} alt="brain" />
                 </div>
             </Tilt>
         </div>
