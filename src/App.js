6dea9252b275ecf91a311ab948f93f30e3b878e2
devin-lynch <devin.l.lynch@gmail.com>
2023-02-03 19:03:28 -0600

    Add background

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank.jsx'
+import ParticlesBg from 'particles-bg';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="circle" bg={true} params={particlesOptions}/>
       <Navigation />
       <Logo />
       <Rank />
