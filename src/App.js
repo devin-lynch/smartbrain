d387ccb722006875c1d09091d5304f1fdae3ef98
devin-lynch <devin.l.lynch@gmail.com>
2023-02-03 18:53:38 -0600

    Add Rank component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank.jsx'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Rank() {
+    return (
+        <div>
+            <div className="white f3">
+                {'Devin, your current rank is...'}
+            </div>
+            <div className="white f1">
+                {'#5'}
+            </div>
+        </div>
+    )
+}
